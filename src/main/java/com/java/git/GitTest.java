ca073f8c23a17da752dd835d4c73c2cd787dd631
zjg <1056274567@qq.com>
2022-01-18 14:42:32 +0800

    first commit

---
+++ src/main/java/com/java/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.java.git;
+
+/**
+ * @program: gitTest
+ * @description:
+ * @Author: admin
+ * @Date: 2022-01-18 14:38
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
